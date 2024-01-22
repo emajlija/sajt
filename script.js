f8143787b4323d309de35593c3086117aae54587
emajlija <emilija.markovic.14.10@gmail.com>
2024-01-22 14:36:46 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const fixedNav = document.querySelector(".header__nav");
+const textType = document.querySelector(".text");
+
+window.addEventListener("scroll", fixNav);
+
+function fixNav() {
+  if (window.scrollY > fixedNav.offsetHeight + 150) {
+    fixedNav.classList.add("active");
+  } else {
+    fixedNav.classList.remove("active");
+  }
+}
