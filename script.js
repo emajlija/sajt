5eb4b02c030a4fc05483986647de062015bd2041
emajlija <emilija.markovic.14.10@gmail.com>
2024-01-24 14:51:16 +0100

    contact commit

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const fixedNav = document.querySelector(".header__nav");
 const textType = document.querySelector(".text");
+const contactBtn = document.querySelector("#contact");
 
 window.addEventListener("scroll", fixNav);
+contactBtn.addEventListener("click", function () {
+  window.open("../pages/contact.html", "_blank");
+});
 
 function fixNav() {
   if (window.scrollY > fixedNav.offsetHeight + 150) {
