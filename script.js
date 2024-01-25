c16ec41a7e881a0116c5de6e5d23bc22a120afaf
emajlija <emilija.markovic.14.10@gmail.com>
2024-01-25 14:44:24 +0100

    one page commit

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-const fixedNav = document.querySelector(".header__nav");
+const fixedNav = document.querySelector(".nav");
 const textType = document.querySelector(".text");
 const contactBtn = document.querySelector("#contact");
 
-window.addEventListener("scroll", fixNav);
-contactBtn.addEventListener("click", function () {
-  window.open("../pages/contact.html", "_blank");
+contactBtn.addEventListener("click", () => {
+  document.querySelector(".contact").scrollIntoView({ behavior: smooth });
 });
+
+window.addEventListener("scroll", fixNav);
 
 function fixNav() {
   if (window.scrollY > fixedNav.offsetHeight + 150) {
