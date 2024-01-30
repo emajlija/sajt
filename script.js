de168966efef1ea36e523cc533eafb01a4fac36f
emajlija <emilija.markovic.14.10@gmail.com>
2024-01-30 14:53:26 +0100

    language commit

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
 const fixedNav = document.querySelector(".nav");
 const textType = document.querySelector(".text");
+const languageSelect = document.getElementById("language-select");
+const srp = document.getElementById("srp");
+const eng = document.getElementById("eng");
 
 window.addEventListener("scroll", fixNav);
+
+languageSelect.addEventListener("change", selectLanguage);
 
 function fixNav() {
   if (window.scrollY > fixedNav.offsetHeight + 150) {
     fixedNav.classList.add("active");
   } else {
     fixedNav.classList.remove("active");
+  }
+}
+
+function selectLanguage() {
+  if (languageSelect.value === "eng") {
+    srp.forEach((elem) => (elem.style.display = "none"));
+    eng.innerText = "XXXXXXXXXXXXXX";
+  } else {
+    eng.forEach((elem) => (elem.style.display = "none"));
   }
 }
