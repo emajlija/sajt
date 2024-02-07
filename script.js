811bf8b91f75bcb9c7896ac0a28524368ba5bc5f
emajlija <emilija.markovic.14.10@gmail.com>
2024-02-07 14:03:36 +0100

    language

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 const fixedNav = document.querySelector(".nav");
 const textType = document.querySelector(".text");
-const languageSelect = document.getElementById("language-select");
-const srp = document.querySelectorAll(".srp");
-const eng = document.querySelectorAll(".eng");
 
 window.addEventListener("scroll", fixNav);
 
@@ -11,5 +8,36 @@ function fixNav() {
     fixedNav.classList.add("active");
   } else {
     fixedNav.classList.remove("active");
+  }
+}
+
+let languageContent = {
+  srp: {
+    "header title": "Lorem ipsum dolor sit amet consectetur",
+    "header text":
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, incidunt ducimus unde consectetur quisquam dolor amet dignissimos minima",
+    "about title":
+      "Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit",
+    "about text":
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, incidunt ducimus unde consectetur quisquam dolor amet dignissimos minima Lorem ipsum dolor sit amet consectetur adipisi",
+    "next about":
+      "TLorem ipsum dolor sit amet consectetur adipisicing elit Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+  },
+  eng: {
+    "header title": "ENG Lorem ipsum dolor sit amet consectetur",
+    "header text":
+      "ENG Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, incidunt ducimus unde consectetur quisquam dolor amet dignissimos minima",
+    "about title":
+      "ENG Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit",
+    "about text":
+      "ENG Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, incidunt ducimus unde consectetur quisquam dolor amet dignissimos minima Lorem ipsum dolor sit amet consectetur adipisi",
+    "next about":
+      "ENG TLorem ipsum dolor sit amet consectetur adipisicing elit Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+  },
+};
+
+function swithcLang(lang) {
+  for (let key in languageContent[lang]) {
+    document.getElementById(key).innerHTML = languageContent[lang][key];
   }
 }
